c0b04a5ceba13f0e333caae6c326f19093baff02
Louis Moses <louismoses.rait@gmail.com>
2023-08-17 13:47:25 +0800

    pass single item to the list

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+app.post("/submit", (req, res) => {
+  const newItem = req.body["product"];
+  res.render("index.ejs", { toBuy: newItem });
+});
 
 app.listen(port, () => {
   console.log(`App server running on port ${port}`);
